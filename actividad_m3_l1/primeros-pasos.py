3ae3e51e457c1fb78869a98e5e5485b1035853f4
AgnusAlfa <umanomedials@gmail.com>
2026-03-19 00:48:20 +0000

    v1

---
+++ actividad_m3_l1/primeros-pasos.py
@@ -0,0 +1,28 @@
+
+# ITEM 1: Imprimir un mensaje inicial
+
+print ("¡Hola! Este es mi primer programa en Python")
+
+
+# ITEM 2: Declarar variables simples
+
+# Declaración de variables (Guardamos la información)
+nombre = "Joao"    # Las comillas indican que es un texto (String)
+edad = 30                   # Sin comillas, porque es un número entero (Integer)
+ciudad = "La Serena"         # Otro texto (String)
+
+# Mostrar el contenido (Usamos la función print)
+print(nombre)
+print(edad)
+print(ciudad)
+
+# ITEM 3: Realizar operaciones básicas
+
+# Declaramos el primer número entero
+numero_a = 15 
+# Declaramos el segundo número entero
+numero_b = 10 
+# Realizamos la operación de suma y asignamos el resultado a una variable
+resultado = numero_a + numero_b 
+# Mostramos el valor del resultado en la consola
+print(resultado)
